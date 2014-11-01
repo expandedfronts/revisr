be80e0316481a0bf62c7bb9013f83cde46cbc233
ExpandedFronts <support@expandedfronts.com>
2014-11-01 04:17:21 -0400

    Cleaned up branches template

--- templates/branches.php
+++ templates/branches.php
@@ -46,23 +46,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 				</thead>
 					<?php
 						$git = new Revisr_Git;
-						$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'local_branches';
-
-						if ( $active_tab == 'remote_branches' ) {
-							$output = $git->get_branches( true );
-						} else {
-							$output = $git->get_branches();
-						}
+						$output = $git->get_branches();
 						
 						if ( is_array( $output ) ) {
 							foreach ($output as $key => $value){
 								
 								$branch 		= substr($value, 2);
 								$num_commits 	= Revisr_Admin::count_commits( $branch );
-
-								if ( $active_tab == 'remote_branches' ) {
-									$num_commits = count( $git->run( "log $branch --pretty=oneline" ) );
-								}
 								
 								if ( substr( $value, 0, 1 ) === "*" ){
 									echo "<tr>
@@ -80,28 +70,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 									$pull_remote_url 	= get_admin_url() . "admin-post.php?action=pull_remote_form&remote_branch={$branch}&TB_iframe=true&width=350&height=200";
 									?>
 									<tr>
-									<td><?php echo $branch; ?></td>
-									<td style='text-align:center;'><?php echo $num_commits; ?></td>
-									<?php
-									if ( $active_tab == 'remote_branches' ) {
-
-										?>
-										<td class="center-td">
-											<a class="button branch-btn thickbox" href="<?php echo $pull_remote_url; ?>" title="<?php _e( 'Pull Remote', 'revisr' ); ?>"><?php _e( 'Pull into...', 'revisr' ); ?></a>
-										</td></tr>
-										<?php
-
-									} else {
-
-										?>
+										<td><?php echo $branch; ?></td>
+										<td style='text-align:center;'><?php echo $num_commits; ?></td>
 										<td class="center-td">
 											<a class='button branch-btn' href='<?php echo $checkout_url; ?>'><?php _e( 'Checkout', 'revisr' ); ?></a>
 											<a class='button branch-btn merge-btn thickbox' href="<?php echo $merge_url; ?>" title="<?php _e( 'Merge Branch', 'revisr' ); ?>">Merge</a>
 											<a class='button branch-btn delete-branch-btn thickbox' href='<?php echo $delete_url; ?>' title='<?php _e( 'Delete Branch', 'revisr' ); ?>'><?php _e( 'Delete', 'revisr' ); ?></a>
-										</td></tr>
-										<?php
-
-									}
+										</td>
+									</tr>
+									<?php
 								}
 							}										
 						}
@@ -115,7 +92,6 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 						</tr>
 					</tfoot>
 				</table>
-				<p><a href="?page=revisr_branches&tab=local_branches"><?php _e( 'Local Branches', 'revisr' ); ?></a> | <a href="?page=revisr_branches&tab=remote_branches"><?php _e( 'Remote Branches', 'revisr' ); ?></a></p>
 			</form>
 		</div><!-- /#col-right -->
 		<div id="col-left">
