50026debf7745177733795e8c4a2a3e7117fc85e
ExpandedFronts <support@expandedfronts.com>
2014-12-06 04:37:26 -0500

    Only check remote URL when the field loses focus

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,13 +1,13 @@
-	var input_field = document.getElementById("remote_url");
-	var result_span = document.getElementById("verify-remote");
+	var remote_field 	= document.getElementById("remote_url");
+	var result_span 	= document.getElementById("verify-remote");
 
-	jQuery(input_field).keyup(function () {
-		var input_value = document.getElementById("remote_url").value;
+	jQuery(remote_field).blur(function() {
+		var input_value = remote_field.value;
 		var data = {
 			action: 'verify_remote',
 			remote: input_value
-
 		};
+
 		jQuery.post(ajaxurl, data, function(response) {
 			if (response.indexOf('Success') !== -1) {
 				result_span.className = "verify-remote-success";
