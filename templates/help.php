e3335c1f9ffb413a3b05ac770c88b4c2c3a36919
revisr <support@expandedfronts.com>
2015-01-14 01:38:33 -0500

    Started on help tab

---
+++ templates/help.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Displays the help page.
+ *
+ * @package   Revisr
+ * @license   GPLv3
+ * @link      https://revisr.io
+ * @copyright 2014 Expanded Fronts, LLC
+ */
+
+?>
+
+<table class="form-table">
+
+	<tbody>
+
+		<tr>
+			<td><label for=""><strong><?php _e( 'Exec() Enabled', 'revisr' ); ?></strong></label></td>
+			<td></td>
+		</tr>
+
+		<tr>
+			<td><label for=""><strong><?php _e( 'Git Version', 'revisr' ); ?></strong></label></td>
+			<td></td>
+		</tr>
+
+		<tr>
+			<td><label for=""><strong><?php _e( 'Git Path', 'revisr' ); ?></label></strong></td>
+			<td></td>
+		</tr>
+
+		<tr>
+			<td><label for=""><strong><?php _e( 'MySQL Path', 'revisr' ); ?></strong></label></td>
+			<td></td>
+		</tr>
+
+		<tr>
+			<td><label for=""><strong><?php _e( 'Permissions', 'revisr' ); ?></strong></label></td>
+			<td></td>
+		</tr>
+
+	</tbody>
+
+</table>
