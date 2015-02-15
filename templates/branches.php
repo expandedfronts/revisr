a73b55ffa2b803f0d1e998b70860a4238cae8d9f
revisr <support@expandedfronts.com>
2015-02-14 21:25:40 -0500

    fixed shorttag

--- templates/branches.php
+++ templates/branches.php
@@ -74,7 +74,7 @@ $git 	= $revisr->git;
 											<a class="button disabled branch-btn" onclick="preventDefault()" href="#"><?php _e( 'Delete', 'revisr' ); ?></a>
 										</td>
 									</tr>
-									<?
+									<?php
 								} else {
 									$checkout_url 		= $admin_url . "admin-post.php?action=process_checkout&branch={$branch}";
 									$merge_url 			= $admin_url . "admin-post.php?action=merge_branch_form&branch={$branch}&TB_iframe=true&width=350&height=200";
