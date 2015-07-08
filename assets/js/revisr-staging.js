5bfbbff02fa5907255294779a27b664c45cba794
Spenser Hale <spenserhale@outlook.com>
2015-07-08 13:27:22 -0700

    replacing double quotes with single quotes

--- assets/js/revisr-staging.js
+++ assets/js/revisr-staging.js
@@ -1,37 +1,37 @@
 function stage_file() {
-    jQuery("#unstaged > option:selected").each(function () {
-        jQuery(this).remove().appendTo("#staged");
+    jQuery('#unstaged > option:selected').each(function () {
+        jQuery(this).remove().appendTo('#staged');
     });
-    jQuery('#count_staged').innerHTML = jQuery("#staged option").length;
+    jQuery('#count_staged').innerHTML = jQuery('#staged option').length;
 }
 
 function unstage_file() {
-    jQuery("#staged > option:selected").each(function () {
-        jQuery(this).remove().appendTo("#unstaged");
+    jQuery('#staged > option:selected').each(function () {
+        jQuery(this).remove().appendTo('#unstaged');
     });
-    jQuery('#count_staged').innerHTML = jQuery("#staged option").length;
+    jQuery('#count_staged').innerHTML = jQuery('#staged option').length;
 }
 
 function stage_all() {
-    jQuery("#unstaged > option").each(function () {
-        jQuery(this).remove().appendTo("#staged");
+    jQuery('#unstaged > option').each(function () {
+        jQuery(this).remove().appendTo('#staged');
     });
-    jQuery('#count_staged').innerHTML = jQuery("#staged option").length;
+    jQuery('#count_staged').innerHTML = jQuery('#staged option').length;
 }
 
 function unstage_all() {
-    jQuery("#staged > option").each(function () {
-        jQuery(this).remove().appendTo("#unstaged");
+    jQuery('#staged > option').each(function () {
+        jQuery(this).remove().appendTo('#unstaged');
     });
-    jQuery('#count_staged').innerHTML = jQuery("#staged option").length;
+    jQuery('#count_staged').innerHTML = jQuery('#staged option').length;
 }
 
 function commit_files() {
-    jQuery("#staged option").each(function() {
-        jQuery(this).attr("selected", "selected");
+    jQuery('#staged option').each(function() {
+        jQuery(this).attr('selected', 'selected');
     });
-    jQuery("#commit").val("Committing...");
-    jQuery(".spinner").show();
+    jQuery('#commit').val('Committing...');
+    jQuery('.spinner').show();
 }
 
 jQuery(document).ready(function($) {
@@ -47,36 +47,36 @@ jQuery(document).ready(function($) {
     });
 
     // empty_title
-    if (url.indexOf("message=42") != "-1") {
-        document.getElementById('message').innerHTML = "<div class='error'><p>" + pending_vars.empty_title_msg + "</p></div>";
+    if (url.indexOf('message=42') != '-1') {
+        document.getElementById('message').innerHTML = '<div class='error'><p>' + pending_vars.empty_title_msg + '</p></div>';
     }
     // empty_commit
-    if (url.indexOf("message=43") != "-1") {
-        document.getElementById('message').innerHTML = "<div class='error'><p>" + pending_vars.empty_commit_msg + "</p></div>";
+    if (url.indexOf('message=43') != '-1') {
+        document.getElementById('message').innerHTML = '<div class='error'><p>' + pending_vars.empty_commit_msg + '</p></div>';
     }
     // error_commit
-    if (url.indexOf("message=44") != "-1") {
-        document.getElementById('message').innerHTML = "<div class='error'><p>" + pending_vars.error_commit_msg + "</p></div>";
+    if (url.indexOf('message=44') != '-1') {
+        document.getElementById('message').innerHTML = '<div class='error'><p>' + pending_vars.error_commit_msg + '</p></div>';
     }
 
 });
 
-jQuery(document).on("dblclick", ".pending", function (event) {
+jQuery(document).on('dblclick', '.pending', function (event) {
     var pending = event.target.value,
         status  = pending.substr(0, 3);
-    if ( status === " M " ) {
-        var file = ajaxurl + "?action=view_diff&file=" + pending.substr(3);
+    if ( status === ' M ' ) {
+        var file = ajaxurl + '?action=view_diff&file=' + pending.substr(3);
         tb_show(pending_vars.view_diff, file);
     }
 });
 
-jQuery(document).on("dblclick", ".committed", function (event) {
+jQuery(document).on('dblclick', '.committed', function (event) {
     var pending = event.target.value,
         commit  = document.getElementById('commit_hash').value,
         status  = pending.substr(0, 2),
         file    = pending.substr(2);
     if (status.indexOf('M') !== -1) {
-        var file = ajaxurl + "?action=view_diff&file=" + file.trim() + "&commit=" + commit;
-        tb_show("View Diff", file);
+        var file = ajaxurl + '?action=view_diff&file=' + file.trim() + '&commit=' + commit;
+        tb_show('View Diff', file);
     }
 });
