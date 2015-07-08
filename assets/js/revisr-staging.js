9a337a0e839163ecada69c3ac7054fbae9959d7b
Spenser Hale <spenserhale@outlook.com>
2015-07-08 13:21:14 -0700

    combining var declarations

--- assets/js/revisr-staging.js
+++ assets/js/revisr-staging.js
@@ -39,13 +39,12 @@ jQuery(document).ready(function($) {
     var data = {
         action: 'pending_files',
         security: pending_vars.ajax_nonce
-    };
+        },
+        url = document.URL;
 
     $.post(ajaxurl, data, function(response) {
         document.getElementById('pending_files_result').innerHTML = response;
     });
-
-    var url = document.URL;
 
     // empty_title
     if (url.indexOf("message=42") != "-1") {
@@ -63,8 +62,8 @@ jQuery(document).ready(function($) {
 });
 
 jQuery(document).on("dblclick", ".pending", function (event) {
-    var pending = event.target.value;
-    var status  = pending.substr(0, 3);
+    var pending = event.target.value,
+        status  = pending.substr(0, 3);
     if ( status === " M " ) {
         var file = ajaxurl + "?action=view_diff&file=" + pending.substr(3);
         tb_show(pending_vars.view_diff, file);
@@ -72,10 +71,10 @@ jQuery(document).on("dblclick", ".pending", function (event) {
 });
 
 jQuery(document).on("dblclick", ".committed", function (event) {
-    var pending = event.target.value;
-    var commit  = document.getElementById('commit_hash').value;
-    var status  = pending.substr(0, 2);
-    var file    = pending.substr(2);
+    var pending = event.target.value,
+        commit  = document.getElementById('commit_hash').value,
+        status  = pending.substr(0, 2),
+        file    = pending.substr(2);
     if (status.indexOf('M') !== -1) {
         var file = ajaxurl + "?action=view_diff&file=" + file.trim() + "&commit=" + commit;
         tb_show("View Diff", file);
