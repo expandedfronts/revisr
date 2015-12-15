100596c802163dab9aefcd3ce3678f29992e491b
Matt Shaw <matt@expandedfronts.com>
2015-12-15 13:27:06 -0500

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        makepot: {
+            target: {
+                options: {
+					domainPath: '/languages/',
+					mainFile: 'revisr.php',
+					potFilename: 'revisr.pot',
+					potHeaders: {
+						poedit: true,
+						'report-msgid-bugs-to': 'http://wordpress.org/support/plugin/slim-seo',
+						'last-translator': 'Revisr <support@expandedfronts.com>',
+						'language-team': 'Revisr <support@expandedfronts.com>'
+					},
+					type: 'wp-plugin',
+					updateTimestamp: true,
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-wp-i18n');
+
+};
